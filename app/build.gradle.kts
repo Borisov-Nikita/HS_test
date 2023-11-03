8a5c956584688b61b8264b124d08978c9cfffcf3
Borisov Nikita <borisov-ta@yandex.ru>
2023-11-03 07:50:11 +0400

    Add gradle version catalog Setup build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,19 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    id("kotlin-kapt")
 }
 
 android {
     namespace = "nik.borisov.hstest"
-    compileSdk = 33
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "nik.borisov.hstest"
-        minSdk = 24
-        targetSdk = 33
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
 
@@ -33,15 +36,32 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //Core
+    implementation(libs.android.appCompat)
+    implementation(libs.android.core)
+    implementation(libs.android.activity)
+    implementation(libs.android.fragment)
+
+    //Coroutines
+    implementation(libs.kotlin.coroutines.core)
+
+    //Lifecycle
+    implementation(libs.android.lifecycle.runtime)
+    implementation(libs.android.lifecycle.viewmodel)
+
+    //Ui
+    implementation(libs.android.constraintLayout)
+    implementation(libs.google.material)
+
+    //Hilt
+    implementation(libs.google.hilt.android)
+    kapt(libs.google.hilt.compiler)
+    kapt(libs.kotlin.metadata)
 }
